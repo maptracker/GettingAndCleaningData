aea33d2191b5b51511f3e2fb8552546bed3bb9a8
Charles Tilford <charles.tilford@bms.com>
2015-11-10 16:42:28 -0500

    Added code to read in a registry markdown and then validate against current state

--- registryExample.R
+++ registryExample.R
@@ -1,21 +1,38 @@
 ## Example usage for registryManager
 source("registryManager.R")
-regman <- createFileRegistry( path = "TestRegistry.md")
+regFile <- "TestRegistry.md" # This is where the registry will be written
+## Create the Registry Manager object:
+regman <- createFileRegistry( path = regFile)
 
-## Will not work if you don't have the added files available...
+## This will throw errors if you don't have the files
+## "HashFunctionBenchmarks.png" and "benchmarkDigest.R" available in
+## the working directory...
 
+## Add HashFunctionBenchmarks.png
 regman$addFile( "HashFunctionBenchmarks.png", "Plot of benchmarks, PNG")
-## Should gripe and not do anything:
+## Try to add it again: Should gripe and not do anything:
 regman$addFile( "HashFunctionBenchmarks.png", "My benchmarks")
+## Add a second file:
 regman$addFile( "benchmarkDigest.R", "Script for generating hashing benchmarks from the digest() function")
 
-## Set some parameters:
+## Set some parameters. These are for human consumption (ie will not
+## be machine parsed)
 regman$param("Experiment Rational", "Compare speeds of hashing algorithms in digest()")
 regman$param("Conclusion", "While SHA-1 is slower than the other algorithms, it is only margnially so, and provides increased collision resistance compared to MD5")
 
 ## Set the introduction block:
 regman$setIntro("Benchmarking using microbenchmark() to compare the relative speeds of the available algorithms in digest(). Comparison is being run to choose a default algorithm for registryManager.")
 
-## Write the file:
+## Write the registry to a file
 regman$writeRegistry()
+
+
+
+## Let's validate the file. Create another manager:
+check <- createFileRegistry( path = regFile)
+## Read in the file we just created
+lines <- check$readRegistry()
+## Run through the registry and verify the existance of each file, and
+## that the hash matches reality.
+check$verifyRegistry()
 
--- registryManager.R
+++ registryManager.R
@@ -22,36 +22,53 @@ createFileRegistry <- function( algo = "sha1", path = "FileRegistry.md" ) {
                      HashAlgorithm = algo )
     registry <- NULL
     introText <- NULL
+    naChar    <- as.character(NA)
+    nferr     <- "As-yet unverified data parsed from registry file"
     
     addFile <- function(file = NA, desc = NA, dir = '.') {
         if (is.na(file)) {
             stop("RegistryManager: Attempt to addfile without file argument")
         }
-        path <- file.path(dir, file);
+        path <- file.path(dir, file)
         if (!file.exists(path)) {
             stop(paste("RegistryManager: File not found! ", path))
-        }
-        hash <- digest( file = path, algo = algo)
-        if (path %in% registry$path) {
-            message(paste("File already added to registry:", path))
-            return(hash)
         }
         if (is.na(desc)) {
             stop("You must provide a description for your file")
         }
+        hash <- digest( file = path, algo = algo)
         sz   <- file.size(path)
-        row <- data.frame(file = file,  dir = dir,
-                          path = path,  size = sz,
-                          desc = desc,  hash = hash)
-        if (is.null(registry)) {
-            registry <<- row
-        } else {
-            registry <<- rbind( registry, row )
-        }
+        row  <- .newRow(file = file,  dir = dir, size = sz,
+                        desc = desc,  hash = hash, verified = TRUE,  error = "")
+        .addRegistryRow( row )
+        ## Return the generated hash value
         hash
     }
 
+    .newRow <- function (file, dir, size = as.double(NA), desc = naChar,
+                         hash = naChar, verified = FALSE,
+                         error = "No error given!") {
+        path <- file.path(dir, file)
+        data.frame(file = file,  dir = dir, path = path,  size = size,
+                   desc = desc,  hash = hash, verified = verified,
+                   error = error, stringsAsFactors = FALSE)
+    }
+
+    .addRegistryRow <- function( row ) {
+        if (is.null(registry)) {
+            registry <<- row
+        } else {
+            if (row$path %in% registry$path) {
+                ## This file is already registered
+                message(paste("File already added to registry:", path))
+            } else {
+                registry <<- rbind( registry, row )
+            }
+        }
+    }
+
     setIntro <- function( text = NA ) introText <<- text
+    getIntro <- function() introText
 
     registryList <- function() registry
 
@@ -115,10 +132,188 @@ createFileRegistry <- function( algo = "sha1", path = "FileRegistry.md" ) {
         message(paste("File registry written to:", regFile))
         close(fh)
     }
+
+    .parenRE <- function(RegExp, text) {
+        ## RegExp in R is not fully fleshed out. This implements a
+        ## hack suggested in the internal documentation to allow
+        ## recovery of text from multiple parenthetical captures
+        m <- lapply(regmatches(text, gregexpr(RegExp, text)), function(e) {
+            regmatches(e, regexec(RegExp, e))
+        })
+        hitList <- m[[1]]
+        ## Return NA if the match failed
+        if (length(hitList) == 0) return(NA)
+        ## Otherwise, extract out the character vector of the matches,
+        ## and exclude the first entry
+        hitList[[1]][-1]
+    }
+
+    readRegistry <- function () {
+        ## Make sure the file exists and is non-empty
+        if (!file.exists(regFile)) {
+             stop(paste("readRegistry() failed to find file:", regFile))
+        } else if (!file.size(regFile)) {
+             stop(paste("readRegistry() found file, but it is empty:", regFile))
+        }
+        message(paste("Reading registry", regFile))
+        fh <- file( regFile, open = "rt")
+        lines <- readLines( fh )
+        close(fh)
+
+        ## Parse the lines from the file
+        lineHandler <- function( line ) { } # Start by doing nothing
+        
+        for (i in seq_len(length(lines))) {
+            line <- lines[i]
+            if (grepl("^\\s*$",line)) {
+                ## Ignore blank lines
+                next
+            } else if (!is.na(m <- .parenRE('## (Registry Description|Parameters|Files) ##', line))) {
+                ## We have just entered a new block
+                message(paste("  Parsing Block:", m))
+                if (m == "Registry Description") {
+                    ## The description is just a (line wrapped) single
+                    ## string. Read it in.
+                    introText <<- NULL
+                    lineHandler <- function(line) {
+                        if (is.null(introText)) {
+                            ## First line of text in the block
+                            setIntro( line )
+                        } else {
+                            ## Extend the line
+                            setIntro( paste(introText, line ))
+                        }
+                    }
+                } else if (m == "Parameters") {
+                    ## Parse out the key/val pairs for the parameters
+                    lineHandler <- function(line) {
+                        if (!any(is.na(kv <- .parenRE('^\\* \\*\\*(.+?)\\*\\* : (.+)$', line)))) {
+                            ## Set the parameter pair
+                            param(tag = kv[1], val = kv[2])
+                        } else {
+                            message(paste("    Unexpected line in parameter block:", line))
+                        }
+                    }
+
+                } else if (m == "Files") {
+                    ## Ok, this is why we have a registry in the first place
+                    ## Parse out the files and their metadata
+                    lineHandler <- function(line) {
+                        if (!any(is.na(dirFile <- .parenRE('^1\\. \\[.+\\]\\((.+)\\/([^\\)]+?)\\)<br>\\s*$', line)))) {
+                            ## We have encountered a new file entry
+                            row  <- .newRow(file  = dirFile[2],
+                                            dir   = dirFile[1],
+                                            error = nferr)
+                            .addRegistryRow( row )
+                        } else {
+                            if (is.null(registry)) {
+                                stop(paste("    ERROR! Encounted a Files line before registry is set up!", line))
+                            }
+                            priorRow = nrow(registry)
+                            if (!any(is.na(hash <- .parenRE('^\\s+`(.+)`\\s*$', line)))) {
+                                ## We have found a hash row
+                                if (is.na(registry$hash[priorRow])) {
+                                    ## Good, the slot is still NA
+                                    registry$hash[priorRow] <<- hash
+                                } else {
+                                    str(registry[[priorRow]])
+                                    stop(paste("    ERROR! Attempt to reset hash value in Files block!", line))
+                                }
+                            } else if (!any(is.na(szDesc <- .parenRE('^\\s+\\*\\*\\s*([0-9\\.]+)\\s*kb\\*\\*\\s+\\*(.+)\\*<br>\\s*$', line)))) {
+                                ## We have found a size/description row
+                                
+                                if (is.na(registry$size[priorRow])) {
+                                    ## Good, the slot is still NA
+                                    registry$size[priorRow] <<- as.double(szDesc[1]) * 1000
+                                } else {
+                                    str(registry[[priorRow]])
+                                    stop(paste("    ERROR! Attempt to reset file size in Files block!", line))
+                                }
+                                if (is.na(registry$desc[priorRow])) {
+                                    ## Good, the slot is still NA
+                                    registry$desc[priorRow] <<-szDesc[2]
+                                } else {
+                                    str(registry[[priorRow]])
+                                    stop(paste("    ERROR! Attempt to reset description in Files block!", line))
+                                }
+                            } else {
+                                message(paste("    Unexpected line in files block:", line))
+                            }
+                        }
+                    }
+
+                } else {
+                    message("    Unrecognized block! No code available!")
+                    lineHandler <- function( line ) {
+                        message(paste("      IGNORED:", line))
+                    }
+                }
+            } else {
+                lineHandler(line)
+            }
+        }
+        if (!is.na(foundAlg <- params$HashAlgorithm)) {
+            ## Capture the algorith as stored in the parameters
+            algo <<- foundAlg
+            message(paste("  Hash algorithm set to", foundAlg))
+        }
+        message("  Done. Parsed", nrow(registry),"files in registry")
+        ## Return the parsed lines, because why not.
+        lines
+    }
+
+    verifyRegistry <- function() {
+        failed <- NULL
+        if (is.null(registry)) {
+            message("Registry is empty, verification passes by default")
+            return(failed)
+        }
+        ## Check each file to assure it is present and has appropriate
+        ## hash digest
+        numFiles <- nrow( registry )
+        for (i in seq_len(numFiles)) {
+            ## Get the row representing this entry:
+            entry  <- registry[i, ]
+            verify <- FALSE
+            error  <- "Error was not set during verification!"
+            path   <- entry$path
+            
+            if (!file.exists(path)) {
+                error <- "File not found"
+            } else {
+                check <- digest( file = path, algo = algo)
+                if (identical(check, entry$hash)) {
+                    verify <- TRUE
+                    error  <- ""
+                } else {
+                    error <- paste("Checksum does not match:", check)
+                }
+            }
+            registry[i, ]$verified <<- verify
+            registry[i, ]$error    <<- error
+            if (!verify) {
+                ## Add failed file rows to return value
+                if (is.null(failed)) {
+                    failed <- registry[i, ]
+                } else {
+                    failed <- rbind(failed, registry[i, ])
+                }
+            } 
+        }
+        if (is.null(failed)) {
+            message("All files in registry have been verified")
+        } else {
+            message(paste("!!", nrow(failed)," files failed verification!"))
+        }
+        failed
+    }
     
     list( addFile = addFile,
          registryList = registryList,
          param = param,
          setIntro = setIntro,
-         writeRegistry = writeRegistry)
+         getIntro = getIntro,
+         writeRegistry = writeRegistry,
+         readRegistry = readRegistry,
+         verifyRegistry = verifyRegistry)
 }
